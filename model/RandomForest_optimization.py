91e1860197e3fb2527b6ba15cd85186f7f15b8ac
Saranya-Balachandran <saranya.balachandran13@gmail.com>
2024-01-22 14:00:35 +0100

    Update RandomForest_optimization.py

--- model/RandomForest_optimization.py
+++ model/RandomForest_optimization.py
@@ -22,7 +22,7 @@ def main():
 	df = pd.read_csv(feat,sep='\t')
 
 	df = df[df['hg_ensembl_id'].notna()]
-	df = df.set_index(['hg_ensembl_id','gene_symbol','hg_ensembl_id_1'])
+	df = df.set_index(['hg_ensembl_id','gene_symbol'])
 
 	df1= df[df['Disease_Association'].isna()]
 
